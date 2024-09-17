9426b20dfe782984b6f87e9c46b87d5eae58f468
amusictheorist <a_millaresthomson@outlook.com>
2024-09-16 21:29:37 -0400

    Refactored code

--- head.js
+++ head.js
@@ -9,8 +9,8 @@ const assertEqual = function(actual, expected) {
 const head = function(arr) {
   if (!Array.isArray(arr) || arr.length === 0) {
     return undefined;
-  } else {
-  return arr[0];}
+  }
+  return arr[0];
 };
 
 // Test cases
