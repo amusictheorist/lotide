b23f75c6e7ab8bbf724359f60658a84f64031ec8
amusictheorist <a_millaresthomson@outlook.com>
2024-09-18 11:54:51 -0400

    First commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1 === arr2) {
+    console.log(`✅✅✅ Comparison Passed: arrays are identical`);
+  } else {
+    console.log(`❌❌❌ Comparison Failed: arrays are not identical`);
+  }
+};
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual(eqArrays([], []), true);
