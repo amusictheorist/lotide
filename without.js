abda4c5b32a39a245e92c181f0328ce61c9e9fb9
amusictheorist <a_millaresthomson@outlook.com>
2024-09-18 17:06:24 -0400

    Refactored code

--- without.js
+++ without.js
@@ -10,24 +10,33 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Comparison Passed: arrays are identical`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌ Comparison Failed: arrays are not identical`);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 const without = function(source, itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
-    if (source[i] !== itemsToRemove[i])
+    let isAMatch = false;
+    for (j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        isAMatch = true;
+      }
+    }
+    if (isAMatch === false) {
       newArray.push(source[i]);
+    }
   }
   return newArray;
 };
-const words = ["hello", "world", "lighthouse"];
-console.log(without([1, 2, 3], [1, 2])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
-console.log(assertArraysEqual(words, without(words, "lighthouse")));
+const actual = without([1, 2, 3], [1, 2]);
+const expected = [5];
+assertArraysEqual(actual, expected);
+// console.log(without([1, 2, 3], [1, 2]));
+// console.log(without(["1", "2", "3"], ["3", 1, 2]));
+
