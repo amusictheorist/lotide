9621c2d5ad021cfd10221080c8cf935a77c2da02
amusictheorist <a_millaresthomson@outlook.com>
2024-09-18 16:59:48 -0400

    Added test cases

--- tail.js
+++ tail.js
@@ -10,6 +10,10 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
+
+const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
