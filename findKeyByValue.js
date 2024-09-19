0b6071ae84b57269c5ccadc6116f4dd81c26c006
amusictheorist <a_millaresthomson@outlook.com>
2024-09-19 10:12:19 -0400

    Defined function and added preliminary test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,18 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKeyByValue = function(obj, val) {
+  
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
