f8c4b9dbe05aa7985043cf61cd05e2439dc940f1
amusictheorist <a_millaresthomson@outlook.com>
2024-09-20 11:10:10 -0400

    Tested with assertArraysEqual

--- map.js
+++ map.js
@@ -18,15 +18,27 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
   return results;
 };
 
-const results1 = map(words, (word) => word[0]);
-console.log(results1);
+const words1 = ["ground", "control", "to", "major", "tom"];
+const words2 = ["some", "worldviews", "are", "spacious"];
+const words3 = ["some", "are", "merely", "spaced"];
+
+const actual1 = map(words1, (word) => word[0]);
+const actual2 = map(words2, (word) => word[1]);
+const actual3 = map(words3, (word) => word[2]);
+
+const expected1 = ["g", "c", "t", "m", "t"];
+const expected2 = ["o", "o", "r", "p"];
+const expected3 = ["m", "e", "r", "a"];
+
+assertArraysEqual(actual1, expected1);
+assertArraysEqual(actual2, expected2);
+assertArraysEqual(actual3, expected3);
