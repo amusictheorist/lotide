09700abf6a8602858393c42cb4044dc6324c1126
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 16:34:35 -0400

    Exported function and created testing file

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Rush", "Rush");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(13, 17);
+module.exports = assertEqual;
