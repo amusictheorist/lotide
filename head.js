9f228a83f0f40bcddc0bef2cd74126494891d78e
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 16:35:01 -0400

    Exported function and created testing file

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(arr) {
   if (!Array.isArray(arr) || arr.length === 0) {
@@ -13,9 +7,4 @@ const head = function(arr) {
   return arr[0];
 };
 
-// Test cases
-assertEqual(head([5, 6, 7]), 5); // Should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Should pass
-assertEqual(head([1]), 1); // Edge case: Single element array
-assertEqual(head([]), undefined); // Edge case: Empty array
-assertEqual(head(), undefined); // Edge case: No array provided
+module.exports = head;
