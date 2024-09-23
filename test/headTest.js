ab4dd00c9cfadd14a278d9a49dc19bc8052f8520
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 18:45:17 -0400

    Fixed small issue

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,3 @@
-const assertEqual = require("../assertEqual");
 const assert = require('chai').assert;
 const head = require("../head");
 
@@ -8,8 +7,8 @@ describe("#head", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
-  it("returns 5 for [5]", () => {
-    assert.strictEqual(head([5]), 5); 
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
