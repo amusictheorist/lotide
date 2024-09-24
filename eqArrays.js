f801739489b80cbf017f60649eeb58edc8fbe98b
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:49:49 -0400

    Refactoring code

--- eqArrays.js
+++ eqArrays.js
@@ -14,7 +14,7 @@ const eqArrays = function(arr1, arr2) {
 
 module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
