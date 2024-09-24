87c4ab298886fd51661bb3b28dd6406b7ab608fc
amusictheorist <a_millaresthomson@outlook.com>
2024-09-24 08:03:48 -0400

    Added flatten func

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const eqArrays = require("./eqArrays");
 const assertArraysEqual = require("./assertArraysEqual");
 const eqObjects = require("./eqObjects");
 const assertObjectsEqual = require("./assertObjectsEqual");
-const countLetters = require("./countLetters")
-const countOnly = require("./countOnly")
-const findKey = require("./findKey")
-const findKeyByValue = require("./findKeyByValue")
-const letterPositions = require("./letterPositions")
-const takeUntil = require("./takeUntil")
-const without = require("./without")
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+const flatten = require("./flatten");
 
 module.exports = {
   head:   head,
@@ -29,5 +30,6 @@ module.exports = {
   findKeyByValue: findKeyByValue,
   letterPositions: letterPositions,
   takeUntil: takeUntil,
-  without: without
+  without: without,
+  flatten: flatten
 };
