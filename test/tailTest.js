2dd2c5a00ca9e0a79fcef5efd8a345f685517c9a
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:03:24 -0400

    Added test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
+const assertEqual = require("../assertEqual");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("returns ['5'] for ['4', '5']", () => {
+    assert.strictEqual(tail(['4', '5']), ['5']); 
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+  });
+  });
