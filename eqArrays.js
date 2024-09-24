f429bca50ca4c2281768c51d8d4dff396f81310d
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:48:11 -0400

    Refactoring code

--- eqArrays.js
+++ eqArrays.js
@@ -13,3 +13,8 @@ const eqArrays = function(arr1, arr2) {
 };
 
 module.exports = eqArrays;
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
