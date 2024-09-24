3819d25830bcef9de5262f162fe09d98bbbfc5f2
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:14:50 -0400

    Fixed bugs

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,14 +4,14 @@ const assertEqual = require("../assertEqual");
 
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), [2, 3]);
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
 
   it("returns ['5'] for ['4', '5']", () => {
-    assert.strictEqual(tail(['4', '5']), ['5']); 
+    assert.deepEqual(tail(['4', '5']), ['5']); 
   });
 
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
   });
   });
