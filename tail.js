5fc0b3479e09e745cde8ea4c10b9a98f64220871
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:37:57 -0400

    Refactoring code

--- tail.js
+++ tail.js
@@ -1,13 +1,13 @@
 const assertEqual = require("./assertEqual");
 
 const tail = function(arr) {
-  if (!Array.isArray(arr) || arr.length === 0) {
-    return undefined;
+  if (arr.length <= 1) {
+    return [];
   }
   return arr.slice(1);
 };
 
 module.exports = tail;
 
-console.log(tail(["Hello", "Lighthouse", "Labs"]));
-console.log(tail([1, 2, 3]));
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]));
+assertEqual(tail([1, 2, 3]));
