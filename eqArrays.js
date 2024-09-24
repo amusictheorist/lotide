c40b666dd7f48d8e35aa929de25d440e6cd11557
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 21:11:09 -0400

    Cleaning up code

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual");
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -13,9 +11,3 @@ const eqArrays = function(arr1, arr2) {
 };
 
 module.exports = eqArrays;
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([], []), true);
