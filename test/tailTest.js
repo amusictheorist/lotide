5744c3ae63f0187d20fcc457ec5effdb2d8a3e44
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 21:21:55 -0400

    Cleaning up code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,6 @@
 const assert = require('chai').assert;
 const tail = require("../tail");
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
-assertEqual(tail([1, 2, 3]).length, 2);
-assertEqual(tail([1]).length, 0);
-assertEqual(tail([]).length, 0);
-
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
