db1f0190f7a96447c91620cfa4f633207c779e0c
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:56:49 -0400

    Refactoring code

--- without.js
+++ without.js
@@ -34,9 +34,9 @@ const without = function(source, itemsToRemove) {
   return newArray;
 };
 
-const actual = without([1, 2, 3], [1, 2]);
-const expected = [5];
-assertArraysEqual(actual, expected);
-// console.log(without([1, 2, 3], [1, 2]));
-// console.log(without(["1", "2", "3"], ["3", 1, 2]));
-
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
