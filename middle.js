e3da0963c163b51c9d7af0de4d4c1f2c8d0837cd
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 21:13:41 -0400

    Cleaning up code

--- middle.js
+++ middle.js
@@ -1,6 +1,3 @@
-const assertArraysEqual = require("./assertArraysEqual");
-const eqArrays = require("./eqArrays");
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -14,10 +11,3 @@ const middle = function(array) {
 };
 
 module.exports = middle;
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
