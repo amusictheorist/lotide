bcf47078f77d833f135eb2e177646f7bc8a89175
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 20:02:10 -0400

    Updated func

--- tail.js
+++ tail.js
@@ -1,7 +1,13 @@
 const assertEqual = require("./assertEqual");
 
 const tail = function(arr) {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return undefined;
+  }
   return arr.slice(1);
 };
 
 module.exports = tail;
+
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+console.log(tail([1, 2, 3]));
