086ddf3b1e6994f40c5a72243f987ceba9beeb9f
amusictheorist <a_millaresthomson@outlook.com>
2024-09-23 21:08:57 -0400

    Cleaning up code

--- tail.js
+++ tail.js
@@ -2,15 +2,10 @@ const assertEqual = require("./assertEqual");
 
 const tail = function(arr) {
   let arrTail = arr
-  if (arrTail.length <= 1) {
+  if (arr.length <= 1) {
     return [];
   }
   return arrTail.slice(1);
 };
 
 module.exports = tail;
-
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
-assertEqual(tail([1, 2, 3]).length, 2);
-assertEqual(tail([1]).length, 0);
-assertEqual(tail([]).length, 0);
